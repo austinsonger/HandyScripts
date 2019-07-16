6891d3f3efcb31fdb770ff854de73453a1a69074
Austin Songer <austinsonger@prevailhealth.com>
2019-07-16 15:44:31 -0500

    Add a few different security items.

--- Linux.Bash.MacOS/MacOS-Specific/Security/verify.and.system.sh
+++ Linux.Bash.MacOS/MacOS-Specific/Security/verify.and.system.sh
@@ -33,6 +33,29 @@ defaults read /Library/Preferences/com.apple.Bluetooth ControllerPowerState
 defaults read com.apple.systemuiserver menuExtras | grep Bluetooth.menu
 
 
+# Enable "Set Time and Data Automatically"
+
+sudo systemsetup -getusingnetworktime 
+
+# Ensure time set is within appropriate limits 
+
+sudo systemsetup -getnetworktimeserver 
+
+# Set an inactivity interval of 20 minutes or less for the screen saver 
+
+defaults -currentHost write com.apple.screensaver idleTime -int 1200
+
+# Secure screen saver corners 
+
+defaults read ~/Library/Preferences/com.apple.dock | grep -i corner
+
+# Quick Lock Out - Hot Corners
+
+defaults write ~/Library/Preferences/com.apple.dock wvous-tr-corner -int 6
+
+
+
+
 
 
 
