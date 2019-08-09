6c5a5f91134932ef38a58f7b9e836df1fa6a8af3
Austin Songer <austinsonger@prevailhealth.com>
2019-08-09 10:26:51 -0500

    New

---
+++ Ruby/passwordstrength.rb
@@ -0,0 +1,20 @@
+unless ARGV[0]
+    puts "You need to include a password to test."
+    puts "Usage: ruby passwordstrength.rb mySuperSecretPassword"
+    exit
+  end
+
+password = ARGV[0]
+word = password.split(//)
+letters = Hash.new(0.0)
+set_size = 96                 
+#62 for alphaNumeric
+#26 for only lowercase or only uppercase, 
+#10 for only digits
+
+word.each do |i|              #Count how many instances of each element there are within the array
+  letters[i] += 1.0
+end
+
+
+## NOT FINISHED
